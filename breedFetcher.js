88249f88ae129e2a4bab8fb51f67cd57593ee8e0
Gerard Compion <gcompion93@gmail.com>
2022-01-20 14:40:03 -0500

    user can specify which breed to search for using command line argument

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,8 @@
 const request = require('request');
+const args = process.argv.slice(2);
 
-request ('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+
+request (`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}` , (error, response, body) => {
  const data = JSON.parse(body);
  console.log(data[0]);
 });
