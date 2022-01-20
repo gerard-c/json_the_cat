d4b3f308c6c7a4f88786466ae96189483e46fb5b
Gerard Compion <gcompion93@gmail.com>
2022-01-20 14:22:25 -0500

    setup request to GET siberian data

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,6 @@
 const request = require('request');
+
+request ('https://api.thecatapi.com/v1/images/search?q=sib', (error, response, body) => {
+  console.log(body);
+  console.log(typeof body);
+});
