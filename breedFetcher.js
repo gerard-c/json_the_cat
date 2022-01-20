09fab2755c49d7503a8dcf5c5e3bc00e1b3792fe
Gerard Compion <gcompion93@gmail.com>
2022-01-20 14:58:01 -0500

    added error handling for failed request and invalid argument

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,8 +2,16 @@ const request = require('request');
 const args = process.argv.slice(2);
 
 
-request (`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}` , (error, response, body) => {
- const data = JSON.parse(body);
- console.log(data[0]);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+  if (error) {
+    console.error('Error: Request failed');
+    return;
+  }
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log('Error: Please input a valid breed of cat and try again');
+    return;
+  }
+  console.log(data[0].description);
 });
 
